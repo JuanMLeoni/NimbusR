c9cfbeba1f348bed3d837f3f62cac83159c58a08
Maximo-Verdondoni <avryux04@gmail.com>
2024-10-24 20:28:20 -0300

    Intentando resolver warning LICENSE

---
+++ R/centros_df.R
@@ -0,0 +1,18 @@
+#' Datos de centros meteorológicos
+#'
+#' Este conjunto de datos contiene información sobre los centros meteorológicos NH0910, NH0472, NH0437,
+#' NH0098 y NH0046
+#'
+#' @docType data
+#' @usage data(centros_df)
+#' @format Un dataframe con `95,681` filas y `35` columnas:
+#' \describe{
+#'   \item{id}{(chr) Nombre del centro meteorológico.}
+#'   \item{fecha}{(date) Fecha de la observación.}
+#'   \item{temperatura_abrigo_150cm}{(dbl) Temperatura abrigo media.}
+#'   \item{temperatura_abrigo_150cm_maxima}{(dbl) Temperatura abrigo máxima}
+#'   \item{temperatura_abrigo_150cm_minima}{(dbl) Temperatura abrigo mínima.}
+#' }
+#' @source Datos ficticios para demostración.
+#' @keywords datasets
+"centros_df"
