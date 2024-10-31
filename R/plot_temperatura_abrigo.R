f89f8b4a4674447d02ae4584f6f21d885d500261
Maximo-Verdondoni <avryux04@gmail.com>
2024-10-30 21:16:10 -0300

    Update documentación

--- R/plot_temperatura_abrigo.R
+++ R/plot_temperatura_abrigo.R
@@ -1,14 +1,25 @@
 #' @title Serie de tiempo temperatura abrigo
 #' @description
 #' Genera un grafico de serie de tiempo que muestra la temperatura abrigo de cada estacion
-#' @param temperature Temperatura en Farenheit (numeric)
+#' @param ... Datasets de los centros a graficar (separados por comas).
 #'
-#' @return Temperatura en Celsius (numeric)
+#' @return Gráfico de la temperatura abrigo.
 #' @export
 #'
 #' @examples
-#' plot_temperatura_abrigo(NH0472)
+#' plot_temperatura_abrigo(NH0472, otro_dataset)
 #' @author JM y VM
-plot_temperatura_abrigo <- function(centros) {
-  #Graficar
+plot_temperatura_abrigo <- function(...) {
+  # Combina los datasets
+  centros_list <- list(...)
+  centros_df <- bind_rows(centros_list, .id = "id")
+
+  # Graficar
+  ggplot(centros_df |>
+           mutate(mes = month(fecha)) |>
+           group_by(id, mes) |>
+           summarise(temperatura_media = mean(temperatura_abrigo_150cm, na.rm = TRUE)) ) +
+    geom_line(aes(x = mes, y = temperatura_media, color = id)) +
+    labs(x = "Mes", y = "Temperatura Media", title = "Serie de Tiempo de Temperatura Abrigo") +
+    theme_minimal()
 }
