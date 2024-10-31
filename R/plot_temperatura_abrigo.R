6ef054028a674f7b2d865438e2bd7e095e00c878
Maximo-Verdondoni <avryux04@gmail.com>
2024-10-30 21:08:58 -0300

    Temperatura Abrigo Grafico

---
+++ R/plot_temperatura_abrigo.R
@@ -0,0 +1,14 @@
+#' @title Serie de tiempo temperatura abrigo
+#' @description
+#' Genera un grafico de serie de tiempo que muestra la temperatura abrigo de cada estacion
+#' @param temperature Temperatura en Farenheit (numeric)
+#'
+#' @return Temperatura en Celsius (numeric)
+#' @export
+#'
+#' @examples
+#' plot_temperatura_abrigo(NH0472)
+#' @author JM y VM
+plot_temperatura_abrigo <- function(centros) {
+  #Graficar
+}
