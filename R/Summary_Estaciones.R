fbde402e54734da5076ac3b8d7042d49ef2c5d65
Maximo-Verdondoni <avryux04@gmail.com>
2024-10-31 15:41:37 -0300

    Summary Funcion

---
+++ R/Summary_Estaciones.R
@@ -0,0 +1,34 @@
+#' @title Summary de temperatura abrigo
+#' @description
+#' La funcion genera una summary para mostrar los estadisticos de los centros que solicites
+#' @param ... Centros metereologicos
+#' @return tibble
+#' @export
+#' @examples
+#' centros <- NimbusR::centros_df
+#' Summary_Temperatura_Abrigo(centros)
+#' @author JM y VM
+Summary_Temperatura_Abrigo = function (...) {
+  Estaciones_lista <- list(...)
+
+  if (!all(sapply(Estaciones_lista, is.data.frame))) {
+    stop("Los parametros deben ser dataframes")
+  }
+
+  datos_combinados <- do.call(rbind, Estaciones_lista)
+
+  required_columns <- c("id", "temperatura_abrigo_150cm")
+  missing_columns <- setdiff(required_columns, names(datos_combinados))
+  if (length(missing_columns) > 0) {
+    stop(paste("Las siguientes columnas faltan en los dataset: ", paste(missing_columns, collapse = ", ")))
+  }
+
+  datos_combinados |>
+    dplyr::group_by(id) |>
+    dplyr::summarise(
+      temperatura_media = mean(temperatura_abrigo_150cm, na.rm = TRUE),
+      temperatura_max = max(temperatura_abrigo_150cm, na.rm = TRUE),
+      temperatura_min = min(temperatura_abrigo_150cm, na.rm = TRUE),
+      temperatura_sd = sd(temperatura_abrigo_150cm, na.rm = TRUE)
+    )
+}
